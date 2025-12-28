f660674446f582373e09101d48695c4ac84dcdb1
lgndluke <github@lgndluke.dev>
2025-12-28 18:20:06 +0100

    docs(events/): Added package-info.java for events/ directory.

---
+++ src/main/java/de/exxcellent/challenge/events/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package containing the projects concrete {@link de.exxcellent.challenge.framework.Event} implementations.
+ *
+ * @author Lukas Jeckle
+ **/
+
+package de.exxcellent.challenge.events;
