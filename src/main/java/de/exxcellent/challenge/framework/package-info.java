69555e5cc0cd74dab6f5f57eb69fee886d79ad52
lgndluke <github@lgndluke.dev>
2025-12-28 17:48:20 +0100

    docs(framework/): Added package-info.java for frameworks/ directory.

---
+++ src/main/java/de/exxcellent/challenge/framework/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The package containing framework classes of the event driven architecture pattern. <br>
+ * <br>
+ * <b>Reference Link:</b> <br>
+ * <a href="https://github.com/iluwatar/java-design-patterns/blob/master/event-driven-architecture/README.md">
+ *     GitHub - Java Design Patterns - Event driven architecture
+ * </a>
+ *
+ * @author Lukas Jeckle
+ **/
+
+package de.exxcellent.challenge.framework;
