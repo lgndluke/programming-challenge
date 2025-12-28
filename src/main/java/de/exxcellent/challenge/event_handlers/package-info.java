da2a7b8689ff4d6c0e86c6b8b1478775a58ccdf2
lgndluke <github@lgndluke.dev>
2025-12-28 18:44:13 +0100

    docs(event_handlers/): Added package-info.java for event_handlers/ directory.

---
+++ src/main/java/de/exxcellent/challenge/event_handlers/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package containing this projects concrete {@link de.exxcellent.challenge.framework.EventHandler} implementations.
+ *
+ * @author Lukas Jeckle
+ **/
+
+package de.exxcellent.challenge.event_handlers;
