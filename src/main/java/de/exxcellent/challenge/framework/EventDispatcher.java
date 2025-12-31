4656e8104c365a6318055eda0064f45ad4a4c524
lgndluke <github@lgndluke.dev>
2025-12-31 13:23:05 +0100

    feat(framework/): Turned EventDispatcher into a thread-safe Singleton.
    Turned the EventDispatcher class into a thread-safe Singleton with lazy-loading, which furthermore utilizes the double-checked locking pattern. This guarantees only one instance is created and continuously reused at runtime. Therefore, simplifying global EventDispatcher accessibility.

--- src/main/java/de/exxcellent/challenge/framework/EventDispatcher.java
+++ src/main/java/de/exxcellent/challenge/framework/EventDispatcher.java
@@ -8,6 +8,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * The EventDispatcher class represents a thread-safe publisher/subscriber component, which routes {@link Event} objects
  * to the {@link EventHandler} registered for the event's concrete type. <br>
  * <br>
+ * Hereby, the class is implemented as a thread-safe Singleton with lazy-loading, which utilizes the double-check
+ * locking pattern. Thereby, it is guaranteed that only one instance of the {@link EventDispatcher} class is created
+ * and continuously reused at runtime. <br>
+ * <br>
  * EventHandlers are internally maintained in a {@link ConcurrentHashMap}, so registrations and event dispatches can
  * safely occur from multiple threads without external synchronization. <br>
  *
@@ -16,7 +20,43 @@ import java.util.concurrent.ConcurrentHashMap;
 public class EventDispatcher
 {
 
+    /**
+     * The {@link EventDispatcher#instance} field is marked volatile to ensure proper visibility across multiple threads.
+     * This ensures that the double-checked locking pattern in {@link EventDispatcher#getInstance()} works correctly.
+     **/
+    private static volatile EventDispatcher instance;
+
     private final Map<Class<? extends Event>, EventHandler<? extends Event>> handlers = new ConcurrentHashMap<>();
+
+    /**
+     * Private constructor to disable external instantiation.
+     **/
+    private EventDispatcher() {}
+
+    /**
+     * @return The Singleton instance of the {@link EventDispatcher} class.
+     **/
+    public static EventDispatcher getInstance()
+    {
+        /*
+         Even though this local variable seems useless, it still can improve performance significantly. (Up to 40%)
+           -> Performance improvement due to less volatile field accesses in cases where the Singleton is instantiated.
+
+         Reference Link: https://en.wikipedia.org/wiki/Double-checked_locking#Usage_in_Java
+         */
+        EventDispatcher result = instance;
+
+        if (result != null)
+            return result;
+
+        synchronized (EventDispatcher.class)
+        {
+            if (instance == null)
+                instance = new EventDispatcher();
+
+            return instance;
+        }
+    }
 
     /**
      * Registers an {@link EventHandler} for a specific event type. <br>
